0b19b2be4c92cf6227a4c9110c32f3ce0929bdb6
helderscrolls <nortehelder@gmail.com>
2019-05-31 23:23:50 +0200

    added routing + css + components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home/Home';
+import History from './components/History/History';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <section className='navbar'>
+          <ul>
+            <li><NavLink exact to="/" activeClassName="active"> Accueil </NavLink></li>
+            <li><NavLink to="/notre-histoire"> Notre Histoire </NavLink></li>
+          </ul>
+        </section>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/notre-histoire" component={History} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/History/History.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './History.css'
+
+class History extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Notre Histoire</h1>
+      </div>
+    )
+  }
+}
+
+export default History
